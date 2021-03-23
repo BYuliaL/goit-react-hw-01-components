2e09f009c05f922398a0300665c3dd6e0735a534
Yulia Bandura <yuliabandura@gmail.com>
2021-03-24 00:48:40 +0200

    random color

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,13 +1,29 @@
 import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 
+const color = function generateRandomColor() {
+  return (
+    'rgb(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ')'
+  );
+};
+
 const Statistics = ({ stats, title }) => (
   <section className={styles.statistics}>
     {title.length > 0 && <h2 className={styles.statistics__title}>{title}</h2>}
 
     <ul className={styles.statistics__statList}>
       {stats.map(({ label, percentage, id }) => (
-        <li className={styles.statistics__item} key={id}>
+        <li
+          className={styles.statistics__item}
+          key={id}
+          style={{ backgroundColor: color() }}
+        >
           <span>{label}</span>
           <span>{percentage}%</span>
         </li>
